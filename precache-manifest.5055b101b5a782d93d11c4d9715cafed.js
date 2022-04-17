3e375ed34950a68486d6764e1fb6cf76757aad43
elspetha5 <elspetha5@users.noreply.github.com>
2022-04-17 00:15:09 +0000

    deploy: a1999d74fa6db4e5d5398f6b7806ee272812e2bb

---
+++ precache-manifest.5055b101b5a782d93d11c4d9715cafed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "88f464740c198f65e5e4",
+    "url": "/kings-mowing-service/static/css/main.9a08ea23.chunk.css"
+  },
+  {
+    "revision": "88f464740c198f65e5e4",
+    "url": "/kings-mowing-service/static/js/main.3368dc6b.chunk.js"
+  },
+  {
+    "revision": "87f1446caf0052e188c3",
+    "url": "/kings-mowing-service/static/js/runtime~main.00559f15.js"
+  },
+  {
+    "revision": "1e9f279ee21154dab3cb",
+    "url": "/kings-mowing-service/static/js/2.5f90e825.chunk.js"
+  },
+  {
+    "revision": "d240635f8d152ed18755",
+    "url": "/kings-mowing-service/static/js/3.ba639867.chunk.js"
+  },
+  {
+    "revision": "488aee24affd0bd379543d944ba3fbf0",
+    "url": "/kings-mowing-service/static/media/kings-mowing-wordmark.488aee24.svg"
+  },
+  {
+    "revision": "8af234c255153705f649a570fce6704f",
+    "url": "/kings-mowing-service/static/media/kings-mowing-icon.8af234c2.svg"
+  },
+  {
+    "revision": "928f7ee91f53d615ef58b8874b022eb5",
+    "url": "/kings-mowing-service/index.html"
+  }
+];
